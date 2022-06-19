3f6e54e11f82209722a6480ca28698edb879fd25
kwrzesni <kamil.wrzesniak@onet.pl>
2022-06-19 22:24:24 +0200

    basic modification

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import string
-import random
-from pyo import *
 from gtts import gTTS
 from pydub import AudioSegment
-import wave
-
+from pydub.playback import play
+from os import listdir
+from os.path import isfile, join
+import random
+from pydub.generators import WhiteNoise
+from pydub.generators import Sine
 
 TESTCASE_SIZE = 5
 SYMBOLS = string.ascii_uppercase + string.digits
@@ -14,16 +16,39 @@ def generate_testcase(n):
     return [random.choice(SYMBOLS) for _ in range(n)]
 
 
-def save_testcase(path, testcase):
-    for i, ch in enumerate(testcase):
-        gTTS(text=ch, lang='en', slow=False).save(path + '/' + str(i) + '.wav')
+def save_testcase(path, test_case):
+    for i, ch in enumerate(test_case):
+        gTTS(text=ch, lang='en', slow=False).save(join(path, str(i) + '.mp3'))
 
 
-testcase = generate_testcase(TESTCASE_SIZE)
-save_testcase('temp', testcase)
+def play_saved_testcase(path):
+    for file in sorted([join(path, f) for f in listdir(path) if isfile(join(path, f))]):
+        sound = AudioSegment.from_mp3(file)
+        play(sound)
 
-spf = AudioSegment.from_file(file = "test.mp3",
-                                  format = "mp3")
-signal = spf.readframes(-1)
 
-x = 2
+def load_testcase(path):
+    result = []
+    for file in sorted([join(path, f) for f in listdir(path) if isfile(join(path, f))]):
+        result.append(AudioSegment.from_mp3(file))
+    return result
+
+
+def modify(test_case):
+    result = AudioSegment.empty()
+    for sound in test_case:
+        noise = WhiteNoise().to_audio_segment(duration=len(sound)) - 15
+        combined = sound.overlay(noise)
+        noise = Sine(random.randint(1, 3)).to_audio_segment(duration=len(sound)) + 2
+        combined = combined.overlay(noise)
+        result += combined
+    return result
+
+
+if __name__ == '__main__':
+    testcase = generate_testcase(TESTCASE_SIZE)
+    print(testcase)
+    save_testcase('temp', testcase)
+    testcase = load_testcase('temp')
+    result = modify(testcase)
+    play(result)
