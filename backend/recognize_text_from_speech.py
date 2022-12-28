578a2199c0bdd9c79144a078164be854c5a7aba3
dkawalecc <dkawalec08@gmail.com>
2022-12-28 18:45:08 +0100

    fix v10

--- backend/recognize_text_from_speech.py
+++ backend/recognize_text_from_speech.py
@@ -6,6 +6,33 @@ import speech_recognition as sr
 dirname = os.path.dirname(__file__)
 LANGUAGES_PATH = os.path.join(dirname, 'words')
 API_KEY = 'AIzaSyAm8_BfXMcAGDXpJpwA--NVTP6U6pKtWkc'
+MAX_DISTANCE = 3
+
+
+def edit_distance(s1, s2):
+    m = len(s1) + 1
+    n = len(s2) + 1
+
+    tbl = {}
+    for i in range(m): tbl[i, 0] = i
+    for j in range(n): tbl[0, j] = j
+    for i in range(1, m):
+        for j in range(1, n):
+            cost = 0 if s1[i - 1] == s2[j - 1] else 1
+            tbl[i, j] = min(tbl[i, j - 1] + 1, tbl[i - 1, j] + 1, tbl[i - 1, j - 1] + cost)
+
+    return tbl[i, j]
+
+
+def response_match_tescase(response, testcase):
+    tokens1 = response.split(' ')
+    tokens2 = testcase.split(' ')
+    if len(tokens1) != len(tokens2):
+        return False
+    for i in range(len(tokens1)):
+        if edit_distance(tokens1[i], tokens2[i]) > MAX_DISTANCE:
+            return False
+    return True
 
 
 def get_languages():
@@ -18,6 +45,9 @@ def recognize(*argv):
     input_path = os.path.join(dirname, input_path)
     output_path = argv[1]
     output_path = os.path.join(dirname, output_path)
+    testcase_path = os.path.join(dirname, "res.txt")
+    with open(testcase_path, 'r') as f:
+        testcase = f.read()
 
     if len(argv) == 3:
         language = argv[2]
@@ -37,7 +67,8 @@ def recognize(*argv):
     else:
         with open(output_path, 'w', encoding='utf-8') as file:
             file.write(speech_recognition['alternative'][0]['transcript'].lower())
-        return speech_recognition['alternative'][0]['transcript'].lower()
+        tmp = speech_recognition['alternative'][0]['transcript'].lower()
 
+        return tmp if response_match_tescase(tmp, testcase) else None
 
 # recognize(sys.argv[1:])
